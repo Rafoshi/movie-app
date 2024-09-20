bb3879d30d2214ede8593138c09f3b9152cf4077
Rafoshi <rafael.pereira@brgaap.com>
2024-09-20 18:27:08 -0300

    Cores

--- lib/common/utils.dart
+++ lib/common/utils.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 const imageUrl = 'https://image.tmdb.org/t/p/w500';
-const kBackgoundColor = Color(0xff121012);
+const kBackgoundColor = Color(0xff050844);
 const kbuttonColor = Color.fromARGB(255, 89, 54, 133);
 const apiKey = 'd57a99c45a06241eb5041ad1303275e6';
-Color backgroundPrimary = Color.fromARGB(255, 171, 169, 180);
+Color backgroundPrimary = const Color.fromARGB(255, 171, 169, 180);
